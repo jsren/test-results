14f71376f45fd9744557c949da26efef7ec5b9f6
jsren <jsrenwick3@gmail.com>
2017-07-26 01:58:24 +0100

    f7742063-87ef-4d44-8ff4-94199995498f

---
+++ Build OSTest.hs
@@ -0,0 +1,63 @@
+--Beginning setup stage of Build OSTest@master
+--
+---------------------------------------------------------------------------
+--git clone -v --recurse-submodules --depth 1 --single-branch --shallow-submodules -o master  -- git@github.com:jsren/ostest acdf831c-69a1-47b9-ad97-d43ea8581dbc
+--
+Cloning into 'acdf831c-69a1-47b9-ad97-d43ea8581dbc'...
+
+--
+--Beginning build stage of Build OSTest@master
+--
+---------------------------------------------------------------------------
+--bash -c "make example"
+--
+--g++ -c -Wall -Wextra -fno-sized-deallocation -DOSTEST_STD_EXCEPTIONS -O3 -std=c++11 ostest.cpp -o ostest.o
+--g++ -Wall -Wextra -O3 -std=c++11 ostest.o example.cpp -o example.exe
+--
+
+--Beginning test stage of Build OSTest@master
+--
+---------------------------------------------------------------------------
+--bash -c ./example.exe
+--
+--
+--ostest example application
+----------------------------------------
+--Version 1.4 (c) 2017 James S Renwick
+--Built with:  OSTEST_STD_EXCEPTIONS
+--
+--[PASS] [EmptySuite::EmptyTest] at example.cpp:40
+--	[PASS] [example.cpp:43] "1 == 1"
+--	[PASS] [example.cpp:44] "(1) == (1)"
+--	[PASS] [example.cpp:47] "1 != 2"
+--	[PASS] [example.cpp:48] "(1) != (2)"
+--
+--[FAIL] [CustomSuite::TestWithLoop] at example.cpp:51
+--	[PASS] [example.cpp:53] "(this->testInt) != 0"
+--	[PASS] [example.cpp:63] "this->testInt % 2 == 0"
+-- FIRST: [FAIL] [example.cpp:63] "this->testInt % 2 == 0"
+--	[PASS] [example.cpp:63] "this->testInt % 2 == 0"
+--	[PASS] [example.cpp:58] "(this->testInt) != 0"
+-- FINAL: [FAIL] [example.cpp:63] "this->testInt % 2 == 0"
+--	[PASS] [example.cpp:67] "(this->testInt) == 0"
+--
+--[FAIL] [CustomSuite::TestWithLoopBreak] at example.cpp:71
+--	[PASS] [example.cpp:73] "(this->testInt) != 0"
+--	[PASS] [example.cpp:85] "expr"
+--	[PASS] [example.cpp:78] "(this->testInt) != 0"
+-- FIRST: [FAIL] [example.cpp:81] "this->testInt % 2 == 0"
+-- FINAL: [FAIL] [example.cpp:89] "(this->testInt) == 0"
+--
+--[FAIL] [CustomSuite::ExceptionTest] at example.cpp:95
+--  ONLY: [FAIL] [example.cpp:95] "<unhandled exception>"
+--	   An unhandled exception occurred: This is a test exception.
+--
+--[FAIL] [CustomSuite::ScopedTest] at example.cpp:111
+--	[PASS] [example.cpp:113] "true"
+-- FIRST: [FAIL] [example.cpp:114] "false"
+--	[PASS] [example.cpp:115] "true"
+-- FINAL: [FAIL] [example.cpp:116] "false"
+--	[PASS] [example.cpp:117] "true"
+--
+--
+
